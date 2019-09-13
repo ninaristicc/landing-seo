57e34c6ccd423d1cc3f68406a7839983a10b39d6
ninaristicc <ninaristic@hotmail.com>
2019-09-12 17:16:32 -0700

    font

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 import ImageTwo from "../components/image2"
 import Layout from "../components/layout"
-import ImageThree from "../components/image3"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -10,8 +9,8 @@ const IndexPage = () => (
     <SEO title="Home" />
     <section className="simplepage">
     <div className="content">
-    <h1>COMING SOON...</h1>
-    <p>Stay tuned!</p>
+    <h1>Great things takes time...</h1>
+    <p>coming soon!</p>
     </div>
     <div class="imgpos" style={{ maxWidth: `300px` }}>
     <ImageTwo /> </div>
