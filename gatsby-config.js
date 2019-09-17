1316ab6360371d6f13d874430ed30d3fc3dadc4d
ninaristicc <ninaristic@hotmail.com>
2019-09-17 14:57:37 -0700

    robots

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `SEO BOSS`,
+    description: `Coming soon`,
     author: `@gatsbyjs`,
+    siteUrl: 'https://www.seoboss.rs'
   },
   plugins: [
+    
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -27,6 +29,15 @@ module.exports = {
         icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-robots-txt`,
+    {
+    resolve: `gatsby-plugin-robots-txt`,
+    options: {
+      host: 'https://www.seoboss.rs',
+      sitemap: 'https://www.seoboss.rs/sitemap.xml',
+      policy: [{ userAgent: '*', disallow: '/' }]
+    },
+  },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
